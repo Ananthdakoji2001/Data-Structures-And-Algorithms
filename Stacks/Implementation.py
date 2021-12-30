98fae8d0bde9969581e5314a97121a794063a2f9
Ananta Kumar Dakoji <96828726+Ananthdakoji2001@users.noreply.github.com>
2021-12-30 21:25:35 +0530

    Update Implementation.py

--- Stacks/Implementation.py
+++ Stacks/Implementation.py
@@ -3,36 +3,35 @@ class Node:
         self.val=val
         self.next=None
 
-        
+
 class stack:
     def __init__(self):
         self.top=None
-        self.bottom=None
         self.length=0
-
-    def peek(self):
-        return self.top.val
 
     def push(self,val):
         new_node=Node(val)
-        if self.top==None:
+        if self.length==0:
             self.top=new_node
-            self.bottom=new_node
         else:
-            top=self.top
+            new_node.next=self.top
             self.top=new_node
-            new_node.next=top
+        self.length+=1
+
+    def pop(self):
+        holding_pinter=self.top
+        self.top=self.top.next
+        self.length-=1
+        return holding_pinter.val
+
+    def peek(self):
+        return self.top.val
 
     def print(self):
         curr=self.top
         while curr!=None:
             print(curr.val)
             curr=curr.next
-
-    def pop(self):
-        holding_pointer=self.top
-        self.top=self.top.next
-        return holding_pointer
       
       
       
